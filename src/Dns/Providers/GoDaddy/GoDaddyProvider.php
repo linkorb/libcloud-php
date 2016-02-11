76296993f38dcc65b459be246d9fad1c20682d1a
Denis Alimov <zloimarine@gmail.com>
2016-02-11 11:11:47 +0600

    errors handling in godaddy dns provider

--- src/Dns/Providers/GoDaddy/GoDaddyProvider.php
+++ src/Dns/Providers/GoDaddy/GoDaddyProvider.php
@@ -21,7 +21,7 @@ class GoDaddyProvider extends Base
             'base_url' => [$this->apiUrl, ['version' => 'v1']],
             'defaults' => [
                 'headers' => [
-                    'Authorization' => 'sso-key '.$this->accessToken.':'.$this->accessTokenSecret,
+                    'Authorization' => 'sso-key ' . $this->accessToken . ':' . $this->accessTokenSecret,
                     'Accept' => 'application/json',
                     'Content-Type' => 'application/json'
                 ]
@@ -31,61 +31,87 @@ class GoDaddyProvider extends Base
 
     public function listZones()
     {
-        $response = $this->httpClient->get('domains');
-        if ($response->getStatusCode() == 200) {
-            return array_map([$this, 'toZone'], json_decode($response->getBody(), true));
+        try {
+            $response = $this->httpClient->get('domains');
+            if ($response->getStatusCode() == 200) {
+                return array_map([$this, 'toZone'], json_decode($response->getBody(), true));
+            }
+        } catch (\Exception $e) {
+            return [];
         }
-        return [];
     }
 
     public function listRecords(Zone $zone)
     {
-        $response = $this->httpClient->get("domains/{$zone->getDomain()}/records");
-        if ($response->getStatusCode() == 200) {
-            $response = json_decode($response->getBody(), true);
-            $records = [];
-            foreach ($response as $r)
-            {
-                $records[] = $this->toRecord($r, $zone);
+        try {
+            $response = $this->httpClient->get("domains/{$zone->getDomain()}/records");
+            if ($response->getStatusCode() == 200) {
+                $response = json_decode($response->getBody(), true);
+                $records = [];
+                foreach ($response as $r) {
+                    $records[] = $this->toRecord($r, $zone);
+                }
+                return $records;
             }
-            return $records;
+        } catch (\Exception $e) {
+            return [];
         }
-        return [];
     }
 
     public function createRecord(Zone $zone, ParameterBag $parameters)
     {
-        $newRecord = $this->formatRecord($parameters);
-        $this->httpClient->patch("domains/{$zone->getDomain()}/records/", ['body' => json_encode($newRecord)]);
+        try {
+            $newRecord = $this->formatRecord($parameters);
+            $this->httpClient->patch("domains/{$zone->getDomain()}/records/", ['body' => json_encode($newRecord)]);
 
-        $id = $newRecord['name'].':'.$newRecord['type'];
-        return new Record($id, $newRecord['name'], $newRecord['type'], $newRecord['data'], $zone, 'go_daddy', $newRecord['ttl']);
+            $id = $newRecord['name'] . ':' . $newRecord['type'];
+            return new Record($id, $newRecord['name'], $newRecord['type'], $newRecord['data'], $zone, 'go_daddy',
+                $newRecord['ttl']);
+        } catch (\Exception $e) {
+
+        }
+
     }
 
     public function getZone($zoneDomain = null)
     {
-        $response = $this->httpClient->get("domains/{$zoneDomain}");
-        if ($response->getStatusCode() == 200) {
-            return $this->toZone(json_decode($response->getBody(), true));
+        try {
+            $response = $this->httpClient->get("domains/{$zoneDomain}");
+            if ($response->getStatusCode() == 200) {
+                return $this->toZone(json_decode($response->getBody(), true));
+            }
+        } catch (\Exception $e) {
+
         }
+
     }
 
     public function updateRecord(Record $record, ParameterBag $parameters)
     {
-        $newRecord = $this->formatRecord($parameters);
-        $this->httpClient->put("domains/{$record->getZone()->getDomain()}/records/{$record->getType()}/{$record->getName()}", ['body' => json_encode($newRecord)]);
+        try {
+            $newRecord = $this->formatRecord($parameters);
+            $this->httpClient->put("domains/{$record->getZone()->getDomain()}/records/{$record->getType()}/{$record->getName()}",
+                ['body' => json_encode($newRecord)]);
 
-        $id = $newRecord['name'].':'.$newRecord['type'];
-        return new Record($id, $newRecord['name'], $newRecord['type'], $newRecord['data'], $record->getZone(), 'go_daddy', $newRecord['ttl']);
+            $id = $newRecord['name'] . ':' . $newRecord['type'];
+            return new Record($id, $newRecord['name'], $newRecord['type'], $newRecord['data'], $record->getZone(),
+                'go_daddy', $newRecord['ttl']);
+        } catch (\Exception $e) {
+        }
+
     }
 
     public function getRecord(Zone $zone, $recordId)
     {
-        $recordId = explode(':', $recordId);
-        $response = $this->httpClient->get("domains/{$zone->getDomain()}/records/{$recordId[1]}/{$recordId[0]}");
-        if ($response->getStatusCode() == 200) {
-            return $this->toRecord(json_decode($response->getBody(), true)[0], $zone);
+        try {
+            $recordId = explode(':', $recordId);
+            $response = $this->httpClient->get("domains/{$zone->getDomain()}/records/{$recordId[1]}/{$recordId[0]}");
+            if ($response->getStatusCode() == 200) {
+                return $this->toRecord(json_decode($response->getBody(), true)[0], $zone);
+            }
+        } catch (\Exception $e) {
         }
+
     }
 
     protected function toZone($response)
@@ -98,7 +124,7 @@ class GoDaddyProvider extends Base
     {
         $type = strtoupper($response['type']);
         $name = $response['name'];
-        $id = $name.':'.$type;
+        $id = $name . ':' . $type;
         return new Record($id, $name, $type, $response['data'], $zone, 'go_daddy', $response['ttl']);
     }
 
@@ -117,14 +143,13 @@ class GoDaddyProvider extends Base
             'ttl' => $ttl
         ];
 
-        if ($type == RecordType::SRV)
-        {
+        if ($type == RecordType::SRV) {
             $newRecord = $newRecord + [
-                'service' => '',
-                'protocol' => '',
-                'port' => '',
-                'weight' => 1
-            ];
+                    'service' => '',
+                    'protocol' => '',
+                    'port' => '',
+                    'weight' => 1
+                ];
         }
 
         return $newRecord;
